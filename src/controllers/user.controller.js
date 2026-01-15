c487d54248c541034d593c8af716b6b5bb572e86
wajiha.abbasi0405@gmail.com <wajiha.abbasi0405@gmail.com>
2026-01-15 12:56:18 +0100

    Created Logout API.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -73,7 +73,32 @@ const loginUser = async (req, res) => {
     }
 }
 
+const logoutUser = async (req, res) => {
+    try {
+        const{email} = req.body;
+
+        const user = await User.findOne({
+            email
+        });
+
+        if (!user) return res.status(404).json({
+            message:"User not found."
+        });
+
+        res.status(200).json({
+            message:"Logout successful"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message:"Internal Server Error", error
+        });
+    }
+    
+}
+
 export{
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
